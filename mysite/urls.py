a3d5082f091fcd46bf17fd3f1fdd4e215a4cc71f
Tanmoy Sarkar <ts741127@gmail.com>
2022-07-16 20:09:22 +0530

    hu[

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,10 +23,9 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("blog.urls")),
+    re_path(r'^files/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += re_path(r'^files/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
